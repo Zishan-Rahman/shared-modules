41dc149e42d0275436636660c1fa98fd32572980
Dallas Strouse <dastrouses@gmail.com>
2023-07-07 23:55:47 -0500

    Update script
    Signed-off-by: Dallas Strouse <dastrouses@gmail.com>

--- update-checker.sh
+++ update-checker.sh
@@ -1,38 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# We take just one file as input, and go over it to update the sources.
+# Args to pass to the data checker
+args=("--update" "--never-fork")
 
-if [ ! -f "$1" ]; then
-  echo "Input file not found!"
-  exit 1
-fi
+# Initialize an array to store file paths
+file_paths=()
 
-# Read the input file line by line
-while IFS= read -r line || [[ -n "$line" ]]; do
+# Iterate over directories next to the script
+for dir in ./*/; do
+  # Search for files with the extensions .yaml, .yml, and .json
+  files=("$dir"*.yaml "$dir"*.yml "$dir"*.json)
 
-  # Trim leading and trailing whitespace from the line
-  line=$(echo "$line" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+  # Iterate over the files
+  for file in "${files[@]}"; do
+    # Check if the file exists and contains "x-checker-data"
+    if [[ -f "$file" ]] && grep -q "x-checker-data" "$file"; then
+      # Add the file path to the array
+      file_paths+=("$file")
+    fi
+  done
+done
 
-  # Skip lines starting with #
-  if [[ $line == \#* ]]; then
-    continue
-  fi
-
+# Print the file paths in the array
+for path in "${file_paths[@]}"; do
   echo "Running data checker on: $line"
 
  # If we're not running in a container, use the Flatpak. Else, assume we're
  # running in a container and call the data checker directly.
   if [[ ! -f /run/.containerenv && ! -f /.dockerenv ]]; then
-   flatpak run --filesystem="$(pwd)" org.flathub.flatpak-external-data-checker $line
+   flatpak run --filesystem="$(pwd)" org.flathub.flatpak-external-data-checker ${args[@]} $path
   else
-   /app/flatpak-external-data-checker --update --never-fork $line
+   /app/flatpak-external-data-checker ${args[@]} $path
   fi
-
-  if [ $? -eq 0 ]; then
-    echo "Checker succeeded!"
-  else
-    echo "No bueno: exit code $?"
-    exit 1
-  fi
-done < "$1"
+done
 
